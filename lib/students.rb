5ddf2360659e92c3263e6226e7c4f6cffd6349ac
Isuru <github email address>
2016-08-30 21:39:56 -0400

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,25 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  "SELECT MAX(students.gpa) from students;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(students.gpa) from students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(students.gpa) as 'AVERAGE' from students"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(students.tardies) as 'Total Tardies' from students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(students.gpa) as 'AVERAGE' from students where students.grade = 9; "
 end
+
+
+# INSERT INTO students (name, grade, gpa, tardies)
+#     VALUES ("John", 11, 3.4, 3);
